df20450b19903ccde960fa8effffa3276a8b97c1
sevenveils <hghamilton@gmail.com>
2017-06-13 20:23:10 +0000

    product details test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
+RSpec.feature "ProductDetails", type: :feature, js: true do
+ # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -17,14 +16,16 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see product details" do
     # ACT
     visit root_path
+
+    first('article.product').find_link('Details »').click
 
     # DEBUG
     save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'article.product-detail'
   end
 end
--- spec/support/images.rb
+++ spec/support/images.rb
@@ -1,3 +1,3 @@
 def open_asset(file_name)
-  File.open(Rails.root.join('db', 'seed_assets', apparel1.))
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
